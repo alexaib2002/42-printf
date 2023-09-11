6000ebd15bb184e7b47ac1e5475e7c58fd8a62ee
aaibar-h <aaibar-h@student.42madrid.com>
2023-09-11 13:24:18 +0200

    Normifixes

--- ft_printf_bonus.h
+++ ft_printf_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf_bonus.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaibar-h <aaibar-h@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: aaibar-h <aaibar-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 22:28:22 by aaibar-h          #+#    #+#             */
-/*   Updated: 2023/09/10 14:11:44 by aaibar-h         ###   ########.fr       */
+/*   Updated: 2023/09/11 13:08:41 by aaibar-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@
 #  define FORM_CVS "cspdiuxX%"
 # endif
 # ifndef FORM_FLAGS
-#  define FORM_FLAGS "# +"
+#  define FORM_FLAGS "# +-.0"
 # endif
-# ifndef FLAG_ALT_FORM // Defines #
+# ifndef FLAG_ALT_FORM
 #  define FLAG_ALT_FORM 0x1
 # endif
-# ifndef FLAG_SPACE // Defines ' '
+# ifndef FLAG_SPACE
 #  define FLAG_SPACE 0x2
 # endif
-# ifndef FLAG_PLUS /// Defines +
+# ifndef FLAG_PLUS
 #  define FLAG_PLUS 0x4
 # endif
 # ifndef STR_FORM_CVS
@@ -40,29 +40,29 @@
 # endif
 
 /**
- * @brief Produces an output based on a given formatted string. Additional 
+ * @brief Produces an output based on a given formatted string. Additional
  * arguments may be passed with varags.
- * 
+ *
  * @param str Formatted string about to be printed
  * @param ... Varargs containing values
  * @return int Uppon success, number of ouptutted characters of `str`, excluding
- * NUL character and including the ones inserted after formatting. 
+ * NUL character and including the ones inserted after formatting.
  * A negative handle will be return if some error is found
  */
 int		ft_printf(const char *str, ...);
 
 /**
  * @brief Writes into stdout any standard string.
- * 
+ *
  * @param str String to be printed.
- * @return int Upon success, returns the characters written, excluding NUL 
+ * @return int Upon success, returns the characters written, excluding NUL
  * terminator.
  */
 int		ft_prints(const char *str);
 
 /**
  * @brief Returns a string after converting the int `n` to hexadecimal.
- * 
+ *
  * @param n Number to be converted
  * @param achr Character to be used as the first character of the hexadecimal
  * @return char* String containing the hexadecimal representation of `n`.
@@ -72,7 +72,7 @@ char	*ft_itoh(unsigned int n, char achr);
 /**
  * @brief Returns a string after converting the int `n` to hexadecimal,
  * prefixed with `0x`.
- * 
+ *
  * @param n Number to be converted
  * @param achr Character to be used as the first character of the hexadecimal
  * @return char* String containing the hexadecimal representation of `n`.
@@ -84,7 +84,7 @@ char	*ft_ultoh(size_t n, char achr);
 /**
  * @brief Returns a string after converting the size_t `n` to hexadecimal,
  * prefixed with `0x`.
- * 
+ *
  * @param n Number to be converted
  * @param achr Character to be used as the first character of the hexadecimal
  * @return char* String containing the hexadecimal representation of `n`.
@@ -92,9 +92,9 @@ char	*ft_ultoh(size_t n, char achr);
 char	*ft_ultoph(size_t n, char achr);
 
 /**
- * @brief Takes a decimal number, and returns a string containing the decimal 
+ * @brief Takes a decimal number, and returns a string containing the decimal
  * representation of the number, after applying the formatter flags.
- * 
+ *
  * @param dec Number to be converted
  * @param flags Variable containing bit flags
  * @return char* Resulting string
@@ -105,7 +105,7 @@ char	*ft_parse_dec_flags(int dec, size_t flags);
  * @brief Takes a hex format character ('x' or 'X') and a hexadecimal number,
  * and returns a string containing the hexadecimal representation of the number,
  * after applying the formatter flags.
- * 
+ *
  * @param fmtc Format character
  * @param hex Number to be converted
  * @param flags Variable containing bit flags
